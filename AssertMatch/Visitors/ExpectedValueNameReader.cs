0cd732be48c7dd00adf25c83959d527565b53287
denis.borovnev <denis.borovnev@gmail.com>
2018-03-29 21:31:42 +0300

    fixed incorrect name of expected value for complext expressions. Just return nothing for these cases

--- AssertMatch/Visitors/ExpectedValueNameReader.cs
+++ AssertMatch/Visitors/ExpectedValueNameReader.cs
@@ -5,21 +5,47 @@ namespace AssertMatch.Visitors
     public class ExpectedValueNameReader : ExpressionVisitor
     {
         string _result;
+        int _visitedNodesCount;
+        int _knownVisitedNodesCount;
+
+        string Result => _visitedNodesCount == _knownVisitedNodesCount ? _result : null;
 
         public static string GetName(Expression expression)
         {
             var nameReader = new ExpectedValueNameReader();
             nameReader.Visit(expression);
-            return nameReader._result;
+            return nameReader.Result;
+        }
+
+        public override Expression Visit(Expression node)
+        {
+            if(node != null)
+            {
+                _visitedNodesCount++;
+            }
+            
+            return base.Visit(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _knownVisitedNodesCount++;
+            return base.VisitConstant(node);
         }
 
         protected override Expression VisitMember(MemberExpression node)
         {
+            _knownVisitedNodesCount++;
             var visitResult = base.VisitMember(node);
 
-            _result += (_result == null ? "" : ".") + node.Member.Name;
+            AppendToName(node.Member.Name);
 
             return visitResult;
+        }
+
+        private void AppendToName(string name)
+        {
+            _result += (_result == null ? "" : ".") + name;
         }
     }
 }
